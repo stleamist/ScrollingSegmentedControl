da8303c2165b1ae1f315f7a7b13a5e72b9cf19ed
Dongkyu Kim <stleamist@me.com>
2018-08-09 19:09:55 +0900

    Initial Commit

---
+++ ScrollingSegmentedControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollingSegmentedControl
+//
+//  Created by 김동규 on 2018. 8. 9..
+//  Copyright © 2018년 Stleam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
